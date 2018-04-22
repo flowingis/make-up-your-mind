d86e59403e891af923c2b896fc7096db8a8ad210
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-04-22 19:07:24 +0200

    added input text

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import './style/app.css'
 
 const updateChart = () => {
   const chart = document.querySelector('app-chart')
-
-  const values = Array.from(document.querySelectorAll('input'))
+  const values = Array.from(
+    document.querySelectorAll('input[role="chart-value-input"')
+  )
     .map(input => input.value)
     .map(value => parseInt(value, 10))
 
   chart.values = values
 }
 
-Array.from(document.querySelectorAll('input')).forEach(input => {
-  input.addEventListener('input', updateChart)
-})
+Array.from(document.querySelectorAll('input[role="chart-value-input"')).forEach(
+  input => {
+    input.addEventListener('input', updateChart)
+  }
+)
 
 window.requestAnimationFrame(updateChart)
