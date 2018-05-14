5ee9b7e114215ce2db1c9e8238cab003fe452f44
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-14 09:19:22 +0200

    external access for webpack dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,5 +3,8 @@ const commonConfiguration = require('./webpack.config.common')
 module.exports = {
   ...commonConfiguration,
   mode: 'development',
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  devServer: {
+    host: '0.0.0.0'
+  }
 }
