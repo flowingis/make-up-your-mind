34b9d857baade0e5a4c9f4bfbeed957c7d0ab1fd
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-24 22:50:01 +0200

    Using buttons to add and remove post-it

--- apps/options-board/index.js
+++ apps/options-board/index.js
@@ -1,13 +1,45 @@
 import './style/index.scss'
 import './components'
+import { createAttributesObserver } from './utils/dom'
 
-window.requestAnimationFrame(() => {
-  const board = document.querySelector('app-board')
+const syncChartToAnchor = board => {
+  window.requestIdleCallback(() => {
+    document.querySelector(
+      'a'
+    ).href = `data:image/svg+xml;base64,\n${window.btoa(board.innerHTML)}`
+  })
+}
 
+const onAddClick = board => {
   board.data = [
+    ...board.data,
     {
       x: 200,
       y: 400
     }
   ]
+}
+
+const onRemoveClick = board => {
+  const newData = [...board.data]
+  newData.pop()
+  board.data = newData
+}
+
+window.requestAnimationFrame(() => {
+  const board = document.querySelector('app-board')
+  const addButton = document.querySelector('button[data-add]')
+  const removeButton = document.querySelector('button[data-remove]')
+
+  addButton.addEventListener('click', () => {
+    onAddClick(board)
+  })
+
+  removeButton.addEventListener('click', () => {
+    onRemoveClick(board)
+  })
+
+  createAttributesObserver(board, () => {
+    syncChartToAnchor(board)
+  })
 })
