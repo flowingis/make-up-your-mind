ab437fbf989fc136ff2eae76ca5cbd69dc5f9feb
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-24 22:17:09 +0200

    managing data

--- apps/options-board/components/Board/Board.js
+++ apps/options-board/components/Board/Board.js
@@ -11,11 +11,15 @@ const DEFAULT_POST_IT_ATTRS = {
   height: 76
 }
 
-const createPostIt = ({ x, y, color, attrs = {} }) => {
+const createPostIt = ({ x, y, color, index, attrs = {} }) => {
+  console.log(x, y)
+  const group = document.createElementNS(SVG_NS_URI, 'g')
+
   const postIt = document.createElementNS(SVG_NS_URI, 'rect')
   postIt.setAttribute('x', x)
   postIt.setAttribute('y', y)
   postIt.setAttribute('fill', color)
+  postIt.setAttribute('data-index', index)
 
   const currentAttrs = {
     ...DEFAULT_POST_IT_ATTRS,
@@ -25,7 +29,10 @@ const createPostIt = ({ x, y, color, attrs = {} }) => {
   Object.keys(currentAttrs).forEach(attributeName => {
     postIt.setAttribute(attributeName, currentAttrs[attributeName])
   })
-  return postIt
+
+  group.appendChild(postIt)
+
+  return group
 }
 
 const COLORS = [
@@ -37,11 +44,11 @@ const COLORS = [
   '#FFD333'
 ]
 
-const getMousePosition = (svg, evetn) => {
+const getMousePosition = (svg, event) => {
   var CTM = svg.getScreenCTM()
   return {
-    x: (evetn.clientX - CTM.e) / CTM.a,
-    y: (evetn.clientY - CTM.f) / CTM.d
+    x: Math.floor((event.clientX - CTM.e) / CTM.a),
+    y: Math.floor((event.clientY - CTM.f) / CTM.d)
   }
 }
 
@@ -54,6 +61,7 @@ class Board extends HTMLElement {
     this.startDrag = this.startDrag.bind(this)
     this.onDrag = this.onDrag.bind(this)
     this.endDrag = this.endDrag.bind(this)
+    this.render = this.render.bind(this)
   }
 
   static get observedAttributes () {
@@ -92,30 +100,51 @@ class Board extends HTMLElement {
   }
 
   endDrag (event) {
+    if (this.currentDraggable) {
+      console.log(this.data)
+      const newData = [...this.data]
+      const currentIndex = parseInt(
+        this.currentDraggable.getAttribute('data-index')
+      )
+      const x = parseInt(this.currentDraggable.getAttribute('x'))
+      const y = parseInt(this.currentDraggable.getAttribute('y'))
+
+      newData[currentIndex] = {
+        ...newData[currentIndex],
+        x,
+        y
+      }
+
+      this.data = newData
+
+      console.log(this.data)
+    }
+
     this.currentDraggable = undefined
     this.currentOffset = undefined
   }
 
   render () {
     this.innerHTML = TEMPLATE
-    this.querySelector('svg').addEventListener('load', event => {
-      this.svg = event.target
 
-      this.svg.addEventListener('mousedown', this.startDrag)
-      this.svg.addEventListener('mousemove', this.onDrag)
-      this.svg.addEventListener('mouseup', this.endDrag)
-      this.svg.addEventListener('mouseleave', this.endDrag)
+    this.svg = this.querySelector('svg')
 
-      for (let i = 0; i < 4; i++) {
-        const postIt = createPostIt({
-          x: 200 * (i + 1),
-          y: 400,
-          color: COLORS[i % COLORS.length]
+    this.svg.addEventListener('mousedown', this.startDrag)
+    this.svg.addEventListener('mousemove', this.onDrag)
+    this.svg.addEventListener('mouseup', this.endDrag)
+    this.svg.addEventListener('mouseleave', this.endDrag)
+
+    this.data
+      .map((element, index) =>
+        createPostIt({
+          ...element,
+          index,
+          color: COLORS[index % COLORS.length]
         })
-
+      )
+      .forEach(postIt => {
         this.svg.appendChild(postIt)
-      }
-    })
+      })
   }
 
   connectedCallback () {
--- apps/options-board/index.js
+++ apps/options-board/index.js
@@ -1,2 +1,13 @@
 import './style/index.scss'
 import './components'
+
+window.requestAnimationFrame(() => {
+  const board = document.querySelector('app-board')
+
+  board.data = [
+    {
+      x: 200,
+      y: 400
+    }
+  ]
+})
