bfb49f8800e4c862f03b20453b16d93e5133d1b4
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-28 18:19:58 +0200

    added text

--- apps/options-board/components/Board/postItNodeFactory.js
+++ apps/options-board/components/Board/postItNodeFactory.js
@@ -4,23 +4,36 @@ const DRAGGABLE_CLASS = 'draggable'
 const HEIGHT = 76
 const WIDTH = 127
 
-export default ({ x, y, color, index, attrs = {} }) => {
-  const group = document.createElementNS(SVG_NS_URI, 'g')
-
-  group.setAttribute('class', DRAGGABLE_CLASS)
-  group.setAttribute('transform', encodeTransformAttribute({ x, y }))
-
+const createPostItNode = (color, index) => {
   const postIt = document.createElementNS(SVG_NS_URI, 'rect')
   postIt.setAttribute('fill', color)
   postIt.setAttribute('height', HEIGHT)
   postIt.setAttribute('width', WIDTH)
   postIt.setAttribute('data-index', index)
+  return postIt
+}
 
-  Object.keys(attrs).forEach(attributeName => {
-    postIt.setAttribute(attributeName, attrs[attributeName])
-  })
+const createTextNode = index => {
+  const textNode = document.createElementNS(SVG_NS_URI, 'text')
 
-  group.appendChild(postIt)
+  textNode.setAttribute('alignment-baseline', 'middle')
+  textNode.setAttribute('text-anchor', 'middle')
+  textNode.setAttribute('x', WIDTH / 2)
+  textNode.setAttribute('y', HEIGHT / 2)
+  textNode.setAttribute('font-size', 20)
+
+  textNode.appendChild(document.createTextNode(index + 1))
+  return textNode
+}
+
+export default ({ x, y, color, index }) => {
+  const group = document.createElementNS(SVG_NS_URI, 'g')
+
+  group.setAttribute('class', DRAGGABLE_CLASS)
+  group.setAttribute('transform', encodeTransformAttribute({ x, y }))
+
+  group.appendChild(createPostItNode(color, index))
+  group.appendChild(createTextNode(index))
 
   return group
 }
